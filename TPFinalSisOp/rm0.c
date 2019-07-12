84e73f34aced4dd47d0d6d13aa01ecf7158cd66f
EscobarMariaSol <mariasolescobar21@gmail.com>
2019-07-12 14:21:58 -0300

    implementación de rm0

--- TPFinalSisOp/rm0.c
+++ TPFinalSisOp/rm0.c
@@ -1,11 +1,35 @@
 #define _POSIX_C_SOURCE 200809L
 
-#include <...>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
 
 void rm0(const char *file) {
-    // ...
+	
+	struct stat statbuf;
+	
+	stat(file, &statbuf);
+	
+	if (S_ISDIR(statbuf.st_mode))
+		printf("rmo0: Error. Se está intentando eliminar un  directorio.\n");
+	
+	if (S_ISREG(statbuf.st_mode)) {
+		
+		int res;
+		
+		if ((res = unlink(file)))
+			printf("rm0: unlink error, %d.\n", res);
+		
+	}
 }
 
 int main(int argc, char *argv[]) {
+	
+	if (argc != 2)
+		printf("Error: ingresar el nombre de un único archivo.\n");
+	
     rm0(argv[1]);
+    return 0;
+    
 }
