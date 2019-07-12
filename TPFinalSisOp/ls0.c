14f6191ba69554c16dd2f7a50d4ab8404eaf7ba7
nazar9318 <fedelias93@gmail.com>
2019-07-12 17:12:58 -0300

    Subo ls0, con pequeño error en la implementacion a revisar

---
+++ TPFinalSisOp/ls0.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <dirent.h>
+
+/*Se pide solamente devolver los nombres de los archivos del directorio actual*/
+void ls0(){
+	char nombreDirectorio[1024];
+	getcwd(nombreDirectorio, 1024);
+
+	int error;
+
+	DIR *directorioAbierto = opendir((const char*)nombreDirectorio);
+
+	struct dirent *directorioLeido = readdir(directorioAbierto);
+
+	while(directorioLeido != NULL){
+		printf("%s\n",directorioLeido->d_name);
+		directorioLeido = readdir(directorioAbierto);
+	}
+	/*if(error = closedir(directorioLeido) == -1){
+		panic("Error al cerrar directorio");
+	}*/
+}
+
+int main(){
+	ls0();
+	return 0;
+}
