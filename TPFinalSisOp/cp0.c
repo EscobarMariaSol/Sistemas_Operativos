d5c7013289288276378c6c802ea0f5f99265b5cb
EscobarMariaSol <mariasolescobar21@gmail.com>
2019-07-16 22:50:56 -0300

    modifico cp1 y cp0

--- TPFinalSisOp/cp0.c
+++ TPFinalSisOp/cp0.c
@@ -1,19 +1,20 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 // Pre-condición, el archivo a copiar existe y es regular, el archivo destino no existe
-void cp0(const char *src){
+void cp0(const char *src, const char *dst){
 	int fd1;
 	
 	if ((fd1 = open(src, O_RDONLY)) < 0) {
-		printf("cp0: Error, no se pudo abrir el archivo %s.\n", src);
+		printf("cp0: Error abriendo el archivo %s.\n", src);
 		return;
 	}else{
 		char c;
 		int fd2;
-		if((fd2 = open("Copia.txt", O_WRONLY | O_APPEND | O_CREAT, 0644)) < 0){
-			printf("cp0: Error, no se pudo crear el archivo %s.\n", "Copia.txt");
+		if((fd2 = open(dst, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU)) < 0){
+			printf("cp0: Error creando  el archivo %s.\n", dst);
 			return;
 		}
 
@@ -26,10 +27,10 @@ void cp0(const char *src){
 }
 
 int main(int argc, char *argv[]){
-	if(argc!=2 ){
-		printf("stat0: Error, ingrese un único archivo.\n");
+	if(argc != 3 ){
+		printf("stat0: Error, ingrese los nombres de los archivos origen y destino.\n");
 		return 1;
 	}
-	cp0(argv[1]);
+	cp0(argv[1], argv[2]);
 	return 0;
 }
--- TPFinalSisOp/cp1.c
+++ TPFinalSisOp/cp1.c
@@ -4,28 +4,36 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <sys/types.h>
 
 // Pre-condición, el archivo a copiar existe y es regular, el archivo destino no existe
-void cp1(const char *src){
+void cp1(const char *src, const char *dst){
 	struct stat statbuf;
 	stat(src, &statbuf);
 	int fd1;
 	
 	if ((fd1 = open(src, O_RDONLY)) < 0) {
-		printf("cp0: Error, no se pudo abrir el archivo %s.\n", src);
+		printf("cp1: Error al abrir el archivo %s.\n", src);
 		return;
-	}else{
+	}
+	else{
 		int fd2;
-		if((fd2 = open("Copia.txt", O_WRONLY | O_APPEND | O_CREAT, 0644)) < 0){
-			printf("cp0: Error, no se pudo crear el archivo %s.\n", "Copia.txt");
+		if((fd2 = open(dst, O_CREAT|O_RDWR, S_IRWXU)) < 0){
+			printf("cp1: Error creando el archivo %s.\n", dst);
 			return;
 		}
-		ftruncate(fd2, statbuf.st_size);
+		
+		if (lseek (fd2, statbuf.st_size, SEEK_SET) < 0) {
+			printf ("cp1: Error, creando el nuevo archivo.\n");
+			return;
+		}
+		write (fd2, "", 1);
 		char *source = mmap(NULL, statbuf.st_size, PROT_READ, MAP_SHARED, fd1, 0);
-		char *dest = mmap(NULL, statbuf.st_size, PROT_WRITE, MAP_SHARED, fd2, 0);
 
-		//memcpy((void *)dest, (const void *)source, statbuf.st_size);
-
+		char *dest = mmap(NULL, statbuf.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0);
+		
+		memcpy(dest, source, statbuf.st_size);
+		
 		munmap(source, statbuf.st_size);
 		munmap(dest, statbuf.st_size);
 
@@ -35,10 +43,10 @@ void cp1(const char *src){
 }
 
 int main(int argc, char *argv[]){
-	if(argc!=2 ){
-		printf("cp1: Error, ingrese un único archivo.\n");
+	if(argc != 3 ){
+		printf("cp1: Error, ingrese los nombres de los archivos origen y destino.\n");
 		return 1;
 	}
-	cp1(argv[1]);
+	cp1(argv[1], argv[2]);
 	return 0;
 }
